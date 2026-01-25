0284eb9cbd14b50bc2428cdf294c5414f6b4e8f4
priyadarshini-tech-maker <panchanathanpriya40@gmail.com>
2026-01-25 14:30:03 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,14 @@
+// Last updated: 1/25/2026, 2:30:02 PM
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return nums[nums.length - 1];
+    }
+}

62ab2dba187a31e9a2bc4d2bbda220e72e77251e
priyadarshini-tech-maker <panchanathanpriya40@gmail.com>
2026-01-25 14:29:53 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,19 @@
+// Last updated: 1/25/2026, 2:29:51 PM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            count[c - 'a']--;
+        }
+        for (int val : count) {
+            if (val != 0) return false;
+        }
+        
+        return true;
+    }
+}

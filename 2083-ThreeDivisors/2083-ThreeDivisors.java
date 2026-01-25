03f59bdc3f7189cd9780c4ac6efcd93288d7e442
priyadarshini-tech-maker <panchanathanpriya40@gmail.com>
2026-01-25 14:29:14 +0530

    Create 2083-ThreeDivisors

---
+++ 2083-ThreeDivisors/2083-ThreeDivisors.java
@@ -0,0 +1,7 @@
+// Last updated: 1/25/2026, 2:29:12 PM
+class Solution {
+    public boolean isThree(int n) {
+     return (int)Math.sqrt(n)*(int)Math.sqrt(n)==n && java.math.BigInteger.valueOf((int)Math.sqrt(n)).isProbablePrime(20); }
+}
+
+        

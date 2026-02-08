9215e77a4a443fa7202f4480a8a8da0bee6e48ea
priyadarshini-tech-maker <panchanathanpriya40@gmail.com>
2026-02-08 08:20:10 +0530

    Create

---
+++ problem-1770519008961/problem-1770519008961.java
@@ -0,0 +1,15 @@
+// Last updated: 2/8/2026, 8:20:08 AM
+1class Solution {
+2    public List<Long> mergeAdjacent(int[] nums) {
+3        List<Long> stack = new ArrayList<>();
+4        for(int num : nums) {
+5            long current = num;
+6            while(!stack.isEmpty() && stack.get(stack.size() - 1) == current) {
+7                current += stack.remove(stack.size() - 1);
+8            }
+9            stack.add(current);
+10        }
+11        return stack;
+12        
+13    }
+14}

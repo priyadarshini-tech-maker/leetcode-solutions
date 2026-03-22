aa331f1bc0888b4495f23c6aba978c987cd29d65
priyadarshini-tech-maker <panchanathanpriya40@gmail.com>
2026-03-22 08:19:06 +0530

    Create

---
+++ problem-1774147744310/problem-1774147744310.py
@@ -0,0 +1,9 @@
+# Last updated: 3/22/2026, 8:19:04 AM
+1class Solution(object):
+2    def uniformArray(self, nums1):
+3        """
+4        :type nums1: List[int]
+5        :rtype: bool
+6        """
+7        return True
+8        
